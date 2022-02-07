36218e286f27497325efcec631902a247b44776a
Milad Fallah <falahmilad79@gmail.com>
2022-02-07 14:39:03 +0330

    chat.js in client-side switched to vanilla js

--- public/js/chat.js
+++ public/js/chat.js
@@ -1,82 +1,83 @@
-"use strict";
-
-// const socket = io({
+ // const socket = io({    
 //     auth: {
 //         token: 123456,
 //     },
-// });
-const chatNamespace = io("/chat"); //Query DOM
+ // });
 
+ const chatNamespace = io("/chat");
+
+//Query DOM
 const messageInput = document.getElementById("messageInput"),
-  chatForm = document.getElementById("chatForm"),
-  chatBox = document.getElementById("chat-box"),
-  feedback = document.getElementById("feedback"),
-  onlineUsers = document.getElementById("online-users-list"),
-  chatContainer = document.getElementById("chatContainer"),
-  pvChatForm = document.getElementById("pvChatForm"),
-  pvMessageInput = document.getElementById("pvMessageInput"),
-  modalTitle = document.getElementById("modalTitle"),
-  pvChatMessage = document.getElementById("pvChatMessage");
+    chatForm = document.getElementById("chatForm"),
+    chatBox = document.getElementById("chat-box"),
+    feedback = document.getElementById("feedback"),
+    onlineUsers = document.getElementById("online-users-list"),
+    chatContainer = document.getElementById("chatContainer"),
+    pvChatForm = document.getElementById("pvChatForm"),
+    pvMessageInput = document.getElementById("pvMessageInput"),
+    modalTitle = document.getElementById("modalTitle"),
+    pvChatMessage = document.getElementById("pvChatMessage");
+
 const nickname = localStorage.getItem("nickname"),
-  roomNumber = localStorage.getItem("chatroom");
-let socketId; // Emit Events
+    roomNumber = localStorage.getItem("chatroom");
+let socketId;
+// Emit Events
+chatNamespace.emit("login", { nickname, roomNumber });
 
-chatNamespace.emit("login", {
-  nickname,
-  roomNumber
-});
 chatForm.addEventListener("submit", (e) => {
-  e.preventDefault();
+    e.preventDefault();
+    if (messageInput.value) {
+        chatNamespace.emit("chat message", {
+            message: messageInput.value,
+            name: nickname,
+            roomNumber,
+        });
+        messageInput.value = "";
+    }
+});
 
-  if (messageInput.value) {
-    chatNamespace.emit("chat message", {
-      message: messageInput.value,
-      name: nickname,
-      roomNumber
-    });
-    messageInput.value = "";
-  }
-});
 messageInput.addEventListener("keypress", () => {
-  chatNamespace.emit("typing", {
-    name: nickname,
-    roomNumber
-  });
+    chatNamespace.emit("typing", { name: nickname, roomNumber });
 });
+
 pvChatForm.addEventListener("submit", (e) => {
-  e.preventDefault();
-  chatNamespace.emit("pvChat", {
-    message: pvMessageInput.value,
-    name: nickname,
-    to: socketId,
-    from: chatNamespace.id
-  });
-  $("#pvChat").modal("hide");
-  pvMessageInput.value = "";
-}); // Listening
+    e.preventDefault();
+
+    chatNamespace.emit("pvChat", {
+        message: pvMessageInput.value,
+        name: nickname,
+        to: socketId,
+        from: chatNamespace.id,
+    });
+
+    $("#pvChat").modal("hide");
+    pvMessageInput.value = "";
+});
+// Listening
 
 chatNamespace.on("online", (users) => {
-  onlineUsers.innerHTML = "";
+    onlineUsers.innerHTML = "";
 
-  for (const socketId in users) {
-    if (roomNumber === users[socketId].roomNumber) {
-      onlineUsers.innerHTML += `
+    for (const socketId in users) {
+        if (roomNumber === users[socketId].roomNumber) {
+            onlineUsers.innerHTML += `
             <li >
                 <button type="button" class="btn btn-light mx-2 p-2" data-toggle="modal" data-target="#pvChat" data-id=${socketId} data-client=${
-        users[socketId].nickname
-      }
+                users[socketId].nickname
+            }
                 ${users[socketId].nickname === nickname ? "disabled" : ""}>
                 ${users[socketId].nickname}
                     <span class="badge badge-success"> </span>
                 </button>
             </li>
         `;
+        }
     }
-  }
 });
+
 chatNamespace.on("chat message", (data) => {
-  feedback.innerHTML = "";
-  chatBox.innerHTML += `
+    feedback.innerHTML = "";
+    chatBox.innerHTML += `
                         <li class="alert alert-light">
                             <span
                                 class="text-dark font-weight-normal"
@@ -99,25 +100,29 @@ chatNamespace.on("chat message", (data) => {
                             ${data.message}
                             </p>
                         </li>`;
-  chatContainer.scrollTop =
-    chatContainer.scrollHeight - chatContainer.clientHeight;
+    chatContainer.scrollTop =
+        chatContainer.scrollHeight - chatContainer.clientHeight;
 });
-chatNamespace.on("typing", (data) => {
-  if (roomNumber === data.roomNumber)
-    feedback.innerHTML = `<p class="alert alert-warning w-25"><em>${data.name} در حال نوشتن است ... </em></p>`;
-});
-chatNamespace.on("pvChat", (data) => {
-  $("#pvChat").modal("show");
-  socketId = data.from;
-  modalTitle.innerHTML = "دریافت پیام از طرف : " + data.name;
-  pvChatMessage.style.display = "block";
-  pvChatMessage.innerHTML = data.name + " : " + data.message;
-}); //JQuery
 
+chatNamespace.on("typing", (data) => {
+    if (roomNumber === data.roomNumber)
+        feedback.innerHTML = `<p class="alert alert-warning w-25"><em>${data.name} در حال نوشتن است ... </em></p>`;
+});
+
+chatNamespace.on("pvChat", (data) => {
+    $("#pvChat").modal("show");
+    socketId = data.from;
+    modalTitle.innerHTML = "دریافت پیام از طرف : " + data.name;
+    pvChatMessage.style.display = "block";
+    pvChatMessage.innerHTML = data.name + " : " + data.message;
+});
+
+//JQuery
 $("#pvChat").on("show.bs.modal", function (e) {
-  var button = $(e.relatedTarget);
-  var user = button.data("client");
-  socketId = button.data("id");
-  modalTitle.innerHTML = "ارسال پیام شخصی به :" + user;
-  pvChatMessage.style.display = "none";
+    var button = $(e.relatedTarget);
+    var user = button.data("client");
+    socketId = button.data("id");
+
+    modalTitle.innerHTML = "ارسال پیام شخصی به :" + user;
+    pvChatMessage.style.display = "none";
 });
